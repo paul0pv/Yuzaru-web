eb13790d8857b687931d0e542390110259aeefe7
paul0pv <51866185+paul0pv@users.noreply.github.com>
2022-10-15 17:19:23 -0500

    Delete js directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const navIconMenu = document.getElementById('navIconMenu');
-const nav = document.querySelector('.nav');
-
-navIconMenu.addEventListener('click', () => {
-    nav.classList.toggle('nav--open');
-});
